8899e1908a72b09b2561417ccdac04c107c0ed88
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-16 22:36:15 +0200

    first c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print out some text'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+}
