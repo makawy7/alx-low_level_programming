67356e219ea7fedbf83602624d305682c047e589
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-16 22:46:08 +0200

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'printing out some text'
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
