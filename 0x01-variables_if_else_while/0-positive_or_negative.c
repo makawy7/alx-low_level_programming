9f72244eede8bf218724c3e4bf1024a21256dd24
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-17 17:09:30 +0200

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,8 +18,8 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive", n);
 	if (n == 0)
-		puts("%d is zero", n);
+		printf("%d is zero", n);
 	if (n < 0)
-		puts("%d is negative", n);
+		printf("%d is negative", n);
 	return (0);
 }
