ce86f192a270972e68fd15385e4e070e5f5d87d6
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-17 22:54:21 +0200

    while if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+			c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
