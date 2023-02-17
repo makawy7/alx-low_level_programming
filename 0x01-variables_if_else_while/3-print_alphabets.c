44d890569fcaca6393e7ce5558a70128f6fc1a7c
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-17 22:50:58 +0200

    while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+	char b = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (b < 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+
+	putchar('\n');
+	return (0);
+}
