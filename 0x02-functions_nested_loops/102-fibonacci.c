d839871474b3ae08b2206cee32b6797bca4924a0
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-21 23:43:00 +0200

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,16 +14,16 @@ int main(void)
 	first = 1;
 	second = 2;
 	count = 2;
-	
+
 	printf("%ld, ", first);
 	printf("%ld, ", second);
 
-	while (count <= 50)
-	{	
+	while (count <= 49)
+	{
 		tmp = second;
 		second += first;
 		first = tmp;
-		if (count == 50)
+		if (count == 49)
 		{
 			printf("%ld", second);
 		} else
