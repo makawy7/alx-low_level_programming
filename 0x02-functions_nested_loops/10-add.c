8db608c17a10dbe750ecbd5951fa4a5df2d4960c
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-21 22:25:35 +0200

    add(int, int)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - return the sum of two numbers
+ *
+ * @x: input number
+ * @y: input number
+ *
+ * Return:  x + y
+ *
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
