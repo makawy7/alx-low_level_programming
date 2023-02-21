084ed167c711fc0a61b6f927943f96553d0b81bd
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-21 15:32:53 +0200

    int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - return the absolute value of an integer
+ *
+ * @n : input number
+ *
+ * Return: |n|
+ *
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+
+	return (n);
+}
