a04025691723eef355282012f3bc11c04d7efb7b
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-23 14:28:16 +0200

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - pritns straight line
+ *
+ * @n : length of the line
+ *
+ * Return: void
+ *
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n--)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
