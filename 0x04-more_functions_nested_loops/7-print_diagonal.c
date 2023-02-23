6e57993415981161a4f72021f18d4a5c3763dccd
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-23 14:41:39 +0200

    print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,22 @@
  */
 
 void print_diagonal(int n)
-{	
+{
 	int i, j;
 
 	if (n > 0)
 	{
-	for (i = 0; i < n; i++)
-	{
-		for (j = 1; j <= i; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-		}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
 		_putchar('\\');
 		_putchar('\n');
+		}
 	}
-	} else
+	else
 	{
 		_putchar('\n');
 	}
