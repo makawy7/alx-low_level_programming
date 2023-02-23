88be7d84bf8115f9c0e920102532fd87f506b8a5
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-24 00:26:28 +0200

    leap

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
-* Return: 0
-*/
-
 int main(void)
 {
     int month;
     int day;
     int year;
 
-    month = 04;
-    day = 01;
-    year = 1997;
-
+    month = 2;
+    day = 29;
+    year = 1991;
     printf("Date: %02d/%02d/%04d\n", month, day, year);
-
     day = convert_day(month, day);
-
     print_remaining_days(month, day, year);
 
-    return (0);
+    month = 2;
+    day = 29;
+    year = 2427;
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+    day = convert_day(month, day);
+    print_remaining_days(month, day, year);
+
+    month = 6;
+    day = 30;
+    year = 2022;
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+    day = convert_day(month, day);
+    print_remaining_days(month, day, year);
+
+    month = 9;
+    day = 1;
+    year = 2022;
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+    day = convert_day(month, day);
+    print_remaining_days(month, day, year);
+
+    month = 12;
+    day = 31;
+    year = 2022;
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+    day = convert_day(month, day);
+    print_remaining_days(month, day, year);
+
+    return 0;
 }
