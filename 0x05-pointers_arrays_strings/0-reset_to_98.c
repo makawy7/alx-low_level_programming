a93322742b0fbef082354a9d9d4fdc4f983cc91d
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-27 22:57:32 +0200

    *n = 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change the value of a number using it's pointer
+ *
+ * @n: pointer to a number
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
