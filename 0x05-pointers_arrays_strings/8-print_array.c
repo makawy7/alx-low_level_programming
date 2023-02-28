d81ec42e5ab81850de601b18c35e8a778fa49683
abdallah elmekawy <abdallahelmekawy6@gmail.com>
2023-02-28 03:41:04 +0200

    print n elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints half of a string,
+ *
+ * @a: intput array
+ * @n: elements
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *a);
+		a++;
+		if (i != n - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
